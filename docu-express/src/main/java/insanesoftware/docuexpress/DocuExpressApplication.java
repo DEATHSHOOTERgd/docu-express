733ce4086bd859cdbcebbca3d9efca039d954e4d
DEATHSHOOTERgd <giampaolo.delgadoo@ug.edu.ec>
2024-05-19 12:04:52 -0500

    Initial commit

---
+++ docu-express/src/main/java/insanesoftware/docuexpress/DocuExpressApplication.java
@@ -0,0 +1,13 @@
+package insanesoftware.docuexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuExpressApplication.class, args);
+	}
+
+}
---
+++ docu-express/src/test/java/insanesoftware/docuexpress/DocuExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package insanesoftware.docuexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
